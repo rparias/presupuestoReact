da12c6319e2171198d26105e02695f02b0c09165
Ronald Arias <ronald.arias@thoughtworks.com>
2020-03-17 11:51:09 -0500

    Using state to define quantity

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Pregunta() {
+  const [quantity, updateQuantity] = useState(0);
+
+  const definePresupuesto = e => {
+    updateQuantity(parseInt(e.target.value));
+  };
+
+  const addPresupuesto = e => {
+    e.preventDefault();
+
+    //validar
+
+    //continuar con el proceso
+  };
+
   return (
     <div>
       <h2>Coloca tu presupuesto</h2>
-      <form action="">
+      <form onSubmit={addPresupuesto}>
         <input
           type="number"
           className="u-full-width"
           placeholder="Coloca tu presupuesto"
+          onChange={definePresupuesto}
         />
         <input
           type="submit"
