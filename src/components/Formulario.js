48617205de841c016778b408e2bb8762e81ecde5
Ronald Arias <ronald.arias@thoughtworks.com>
2020-03-18 10:58:13 -0500

    Working on Formulario de Gastos

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,21 +1,50 @@
 import React, { useState } from 'react';
 
 function Formulario() {
+  const [descriptionExpense, updateDescriptionExpense] = useState('');
+  const [quantity, updateQuantity] = useState(0);
+
+  const handleDescription = e => {
+    updateDescriptionExpense(e.target.value);
+  };
+  const handleQuantity = e => {
+    updateQuantity(parseInt(e.target.value, 10));
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    //validar
+
+    //construir el gasto
+
+    //pasar el gasto al componente principal
+
+    //resetear el form
+  };
+
   return (
     <div>
       <h2>Agrega tus gastos aqui</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="campo">
           <label>Nombre Gasto</label>
           <input
             type="text"
             className="u-full-width"
             placeholder="Ej. Transporte"
+            value={descriptionExpense}
+            onChange={handleDescription}
           />
         </div>
         <div className="campo">
           <label>Cantidad Gasto</label>
-          <input type="number" className="u-full-width" placeholder="Ej. 300" />
+          <input
+            type="number"
+            className="u-full-width"
+            placeholder="Ej. 300"
+            value={quantity}
+            onChange={handleQuantity}
+          />
         </div>
         <input
           type="submit"
